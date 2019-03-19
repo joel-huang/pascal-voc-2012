4373606e2717398d36d2031859f55e62db6911db
Joel <huang.joel@hotmail.com>
2019-03-19 19:39:20 +0800

    pass scaling constant into NBLoss class

--- plot.py
+++ plot.py
@@ -1,25 +1,20 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-bcetrain = np.concatenate((np.load('train_history_BCE.npy'), np.load('train_history_BCE_100.npy')), axis=None)
-bceval =  np.concatenate((np.load('val_history_BCE.npy'), np.load('val_history_BCE_100.npy')), axis=None)
-bcemap = np.concatenate((np.load('mAP_history_BCE.npy'), np.load('mAP_history_BCE_100.npy')), axis=None)
+bcetrain = np.load('train_history_BCE_lr0.001_sc0.001_model_BCE_40_0.1131.npy')
+bceval =  np.load('val_history_BCE_lr0.001_sc0.001_model_BCE_40_0.1131.npy')
 
 fig, ax = plt.subplots(nrows=1,ncols=1,figsize=(20,20))
-ax2 = ax.twinx()
 
-epoch_list = np.arange(1, 101)
-xticks = np.arange(0, 100, 10)
+epoch_list = np.arange(1, 41)
+xticks = np.arange(0, 40, 10)
 
 ax.set_xticks(xticks)
 ax.set_title("BCELoss")
 ax.set_xlabel("Epoch")
 ax.set_ylabel("Loss")
-ax2.set_ylabel("mAP")
 ax.plot(epoch_list, bcetrain, label="Training loss")
 ax.plot(epoch_list, bceval, label="Validation loss")
-ax2.plot(epoch_list, bcemap, label="mAP", c='g')
 ax.legend(loc='upper right')
-ax2	.legend(loc='lower right')
 
 plt.show()
